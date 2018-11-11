e5b54f792763b43c9d7660889de3f5f51af2fa83
Esaú JC <esau.jc@gmail.com>
2018-11-11 10:40:25 +0100

    add basic search

--- src/pages/WorldSpeeches.js
+++ src/pages/WorldSpeeches.js
@@ -8,7 +8,9 @@ class WorldSpeeches extends Component {
 
   state = {
     speeches: [],
+    speechesSearch: [],
     isLoading: true,
+    search:'',
     // values: queryString.parse(this.props.location.search),
   }
 
@@ -32,6 +34,7 @@ class WorldSpeeches extends Component {
       .then(result => {
         this.setState({
           speeches: result,
+          speechesSearch:result,
           isLoading: false,
           values: {}
         })
@@ -41,16 +44,41 @@ class WorldSpeeches extends Component {
       })
   }
 
+  handleSearch = (event) => {
+    const { speeches } = this.state;
+
+    const result = speeches.filter((speech,index) => {
+      let speechTitle = speech.title.toUpperCase();
+      let speechTag = speech.tag[0].toUpperCase();
+      console.log(speechTag);
+        if (speechTitle.includes(event.target.value.toUpperCase()) || speechTag.includes(event.target.value.toUpperCase())){
+          speech.index= index;
+          // console.log(comida.index);
+          return speech;    
+        }
+       
+    })
+    
+    this.setState({
+      search: event.target.value,
+      // speeches: result,
+      speechesSearch: result,
+
+    })
+  }
+
   render() {
-    const { speeches, isLoading } = this.state;
+    const { speeches, isLoading, search, speechesSearch } = this.state;
     // console.log(this.props.location);
     
 
     return (
       <div>
         <h1>Speaches Search</h1>
-        {isLoading ? <h2>Loading...</h2> : speeches.map((speech, index) => {
-          return <div key={index}>
+        Search: <input type="search" name="search" size="50" value={search} onChange={this.handleSearch}/>
+
+        {isLoading ? <h2>Loading...</h2> : speechesSearch.map((speech, index) => {
+          return <div index={index} key={speech.title}>
             <div><Link key={speech._id} to={`/speeches/${speech._id}`}>{speech.title}</Link></div>
             </div>
         } )}
