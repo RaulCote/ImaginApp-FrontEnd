acdf73b26ebeb6863c82c052f149b9dda0bdc136
RaulCote <raulcote@hotmail.com>
2018-11-11 10:42:07 +0100

    Save.

--- src/pages/SpeechDetail.js
+++ src/pages/SpeechDetail.js
@@ -35,15 +35,10 @@ renderUpdate = () => {
 
     return (
       <div>
-<<<<<<< HEAD
-        {isLoading ? <h1>Loading</h1> : <div><h1>{speech.title}</h1><h2>{speech.owner}</h2></div>} 
-
-=======
         {isLoading ? <h1>Loading</h1> : <div><FormEdit speech={speech}/><h1>{speech.title}</h1>
         <h2>{speech.owner}</h2>
         <h3>{this.props.user._id}</h3></div>}
         
->>>>>>> 255773e27ae32478c96a5f9a1a4ba233f82e76fe
 
         <h2>Speech Detail</h2>   
         <h3>Hello</h3> 
