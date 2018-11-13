70c68438c88455fe562b3a0d86b8ea2b16b895c2
RaulCote <raulcote@hotmail.com>
2018-11-13 15:19:47 +0100

    More CSS done.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,14 @@ class Navbar extends Component {
         {isLogged ? <div>
           {/* <p>username: {this.props.user.username}</p>
           <p>CurrenUserId: {this.props.user._id}</p> */}
-          <button className="logout" onClick={this.props.logout}>Logout</button>
+          <button className="navbar-links logout" onClick={this.props.logout}>Logout</button>
           {/* <Link to='/speeches'>World</Link>
           <Link to='/profile/speeches'>Your Speeches</Link>
           <Link to='/profile/speeches/new'>Create Speeches</Link>
           <Link to='/profile'>Profile</Link> */}
-        </div> : <div>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link>
+        </div> : <div className="login-container">
+          <Link className="navbar-links" to='/login'>Login</Link>
+          <Link className="navbar-links" to='/signup'>Signup</Link>
         </div>
       }
       </div>
